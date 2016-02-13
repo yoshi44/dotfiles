6ec360e57f0eb63f06ea6a75183e8fef71b8e398
Yoshi <yoshiVM@Yoshis-Mac.local>
2016-02-13 18:38:50 +0900

    Add no dot files logic, install logic

--- update.sh
+++ update.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
 function backup(){
+  #echo "$1"
   NOW=`date +%Y%m%d_%H-%M-%S`
   BACKUP_DIR=${HOME}/dotfiles/backup/${NOW}
   mkdir ${BACKUP_DIR}
-  mv ${HOME}/.vimrc ${BACKUP_DIR}/
-  mv ${HOME}/.zprofile ${BACKUP_DIR}/
-  mv ${HOME}/.zshrc ${BACKUP_DIR}/
+  for backup_file in "$1"; do
+    BACKUP_FILE_PATH=${HOME}/${backup_file}
+    if [[ -e ${BACKUP_FILE_PATH} ]]; then
+      mv ${BACKUP_FILE_PATH} ${BACKUP_DIR}/
+    fi
+  done
+  return 0
+}
+
+function install(){
+  # neobundle insatll
+  curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+  sh ./install.sh
   return 0
 }
 
@@ -19,7 +30,9 @@ function create_symbolic_links(){
 }
 
 echo "HOME===[${HOME}]"
-backup
+readonly BACKUP_FILES=(".vimrc" ".zprofile" ".zshrc")
+backup "${BACKUP_FILES[*]}"
+install
 create_symbolic_links
 
 echo "Done!!"
